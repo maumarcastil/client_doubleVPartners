a87f7f3f7de29d432f456417f40019d288697a18
Mauricio Martinez <maumarcastil@gmail.com>
2023-07-21 21:29:36 -0500

    feat: add layout and header

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
+import { Header } from '../components/header'
 
 const Layout = () => {
   return (
-    <div className='flex min-h-screen bg-slate-500'>
-      <div className='mx-auto w-full max-w-[1440px] bg-white '>
-      {<Outlet/>}
+    <div className='flex min-h-screen bg-gray-100'>
+      <div className='mx-auto w-full max-w-[1440px]'>
+        <Header />
+        <Outlet />
       </div>
     </div>
   )
