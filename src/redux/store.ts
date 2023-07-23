c4729f3711c3cf0ae6cb156b1500967b2dd5b500
Mauricio <maumarcastil@gmail.com>
2023-07-23 14:44:54 -0500

    feat: add default middleware

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,16 @@
+
 import { configureStore } from '@reduxjs/toolkit';
-import githubReducer from './slices/githubSlice';
+import githubReducer from './slices/github/githubSlice';
+import apiReducer from './slices/api/apiSlice';
 
 export const store = configureStore({
   reducer: {
     github: githubReducer,
-  }
+    api: apiReducer
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: false,
+  }),
 });
 
 export type RootState = ReturnType<typeof store.getState>
