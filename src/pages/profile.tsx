263edd3089138e3594eef69967f9345d54f004d8
Mauricio <maumarcastil@gmail.com>
2023-07-23 14:47:19 -0500

    feat: add button export and validations

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,25 +1,39 @@
 import React, { useEffect } from 'react'
 import ProfileDetails from '../components/profileDetails'
-import { useAppDispatch } from '../redux/hooks'
+import { useAppDispatch, useAppSelector } from '../redux/hooks'
 import { useParams } from 'react-router-dom'
-import { fetchUser, fetchUserRepos } from '../redux/slices/thunks'
+import { fetchUser, fetchUserRepos } from '../redux/slices/github/thunks'
 import CardRepositories from '../components/cardRepos'
-import { Button } from '@material-tailwind/react'
+import { Button, Spinner } from '@material-tailwind/react'
+import { createUser, fetchUserbyUsername } from '../redux/slices/api/thunks'
+import { GitHubUserDetails } from '../types/users'
+import toast from 'react-hot-toast'
 
 const ProfilePage = () => {
 
   const dispatch = useAppDispatch()
+  const { data } = useAppSelector(state => state.github.user)
+
+  const { user } = useAppSelector(state => state.api)
+
   const { username } = useParams()
 
   useEffect(() => {
     if (username) {
       dispatch(fetchUser(username))
       dispatch(fetchUserRepos(username))
+      dispatch(fetchUserbyUsername(username))
     }
   }, [])
 
   const handleClick = () => {
-    console.log('click')
+    if (user === null && data !== null) {
+      dispatch(createUser(data as GitHubUserDetails))
+    }else {
+      toast.error('this user cannot be exported', {
+        position: 'top-right'
+      })
+    }
   }
 
   return (
@@ -29,8 +43,21 @@ const ProfilePage = () => {
           <ProfileDetails />
 
           <Button
+            className='flex justify-center items-center'
+            disabled={user === undefined || user !== null || !(data as GitHubUserDetails)?.followers}
             onClick={handleClick}
-          >Export Profile</Button>
+          >
+            {user === undefined || !(data as GitHubUserDetails)?.followers
+            ?
+            <Spinner />
+            :
+            user === null
+            ?
+            'Export Profile'
+            :
+            'Profile Exported'
+            }
+          </Button>
         </div>
         <div className='w-full col-span-1 sm:col-span-2'>
           <section>
